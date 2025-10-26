ca6e0e13a162ee2a1f3d6e5782ff443c438d90fe
vjc2026 <vscode.jcv@gmail.com>
2025-10-26 12:18:08 +0800

    Remove app.listen for Vercel serverless - keep only module.exports

--- server.js
+++ server.js
@@ -4580,9 +4580,9 @@ app.delete('/code_analysis/:id', authenticateToken, (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 
 // Export for Vercel serverless
